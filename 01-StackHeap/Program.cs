3315d8cbfe114397bc07261013447f3e80e55128
Haken, Robert <haken@havit.cz>
2018-10-04 17:17:54 +0200

    StackHeap

--- 01-StackHeap/Program.cs
+++ 01-StackHeap/Program.cs
@@ -1,12 +1,64 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace _01_StackHeap
 {
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			// lokální proměnné
+			int a = 0xAAAA;
+			bool b = true;
+			Int64 c = 0xCCCCCCCC;
+			for (int i = 0; i < 0xFF; i++)
+			{
+				MyClass instance = new MyClass(a); // instance třídy -> heap
+			}
+			int r;
+
+			// volání metody
+			Debugger.Break();
+			r = DoSomething(a, null);
+			Debugger.Break();
+		}
+
+		private static int DoSomething(int vstupniParametr, string druhyParametr)
+		{
+			// lokální proměnné
+			int d = 0xDDDD;
+			int e = 0xEEEE;
+
+			// volání dalších metod
+			Debugger.Break();
+			DoSomethingElse();
+			Debugger.Break();
+			DoNothing();
+			Debugger.Break();
+
+			return vstupniParametr + 1;
+		}
+
+		private static void DoSomethingElse()
+		{
+			Debugger.Break();
+		}
+
+		private static void DoNothing()
+		{
+			Debugger.Break();
+		}
+	}
+
+	public class MyClass
+	{
+		private int _field;
+		private int _field2;
+		private int _field3;
+
+		public MyClass(int field)
+		{
+			_field = field;
 		}
 	}
 }

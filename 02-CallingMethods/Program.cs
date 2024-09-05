6b9f20a9b90a702c73d12448bfb61fab17570d90
Robert Haken <haken@havit.cz>
2024-09-05 21:54:19 +0200

    02-CallingMethods - new demo instead of 02-ManagedHeap

---
+++ 02-CallingMethods/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Diagnostics;
+
+namespace Haken.CallingMethods
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			// inicializace
+			int valueTypeVariable1 = 0xAAAA;
+
+			MyClass referenceTypeVariable1 = new MyClass();
+			referenceTypeVariable1.ValueProperty = 1111;
+			referenceTypeVariable1.ReferenceProperty = null;
+
+			Debugger.Break();
+
+			// předání parametrů "hodnotou" (ByVal)
+			MyMethod(valueTypeVariable1, referenceTypeVariable1);
+
+			Debugger.Break();
+
+			// předání parametrů "odkazem" (ByRef)
+			MyMethodWithByRefParameters(ref valueTypeVariable1, ref referenceTypeVariable1);
+
+			Debugger.Break();
+		}
+
+		private static int MyMethod(
+			int valueTypeParameter,
+			MyClass referenceTypeParameter)
+		{
+			Debugger.Break();
+
+			valueTypeParameter++;
+			referenceTypeParameter.ValueProperty++;
+
+			Debugger.Break();
+
+			referenceTypeParameter = new MyClass();
+			referenceTypeParameter.ValueProperty = 2222;
+
+			Debugger.Break();
+
+			return valueTypeParameter;
+		}
+
+		private static void MyMethodWithByRefParameters(
+			ref int valueTypeParameter,
+			ref MyClass referenceTypeParameter)
+		{
+			Debugger.Break();
+
+			valueTypeParameter++;
+			referenceTypeParameter.ValueProperty++;
+
+			Debugger.Break();
+
+			referenceTypeParameter = new MyClass();
+			referenceTypeParameter.ValueProperty = 2222;
+
+			Debugger.Break();
+		}
+	}
+
+	public class MyClass
+	{
+		public int ValueProperty { get; set; }
+
+		public MyClass ReferenceProperty { get; set; }
+	}
+
+	public struct Coordinates
+	{
+		public int X { get; set; }
+
+		public int Y { get; set; }
+	}
+}
